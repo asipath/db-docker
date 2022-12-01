45bc5313184aa065d26a29c82fd92cd654edaffc
Asitha Sampath <raasampath@gmail.com>
2022-12-01 20:32:14 +0530

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,86 @@
+# Changelog
+
+## UNRELEASED
+
+* ...
+
+## v1.1.3
+
+* Add support for MariaDB 10.6 for DDEV
+* Fix #25: Return an exit code
+* Assume tag is 'latest' if the current branch is 'main'
+
+## v1.1.2
+
+* List ddev as a db-source by @skippednote in #21
+* Add support for GitHub URLs by @skippednote in #23
+
+## v1.1.1
+
+* Set db-source to file when db-file option is present. Fixes #17.
+
+## v1.1.0
+
+* Update documentation for DDEV support
+* Fix #15: Add support for DDEV
+* Remove an invalid SQL statement in truncate caches file
+* Fix the ARG order in Dockerfile
+
+## v1.0.1
+
+* Add support for symfony/process 2.8. Fixes #12
+
+## v1.0.0
+
+* Set a high timeout value so that docker build and push have enough time
+* Add documentation for docker-base section
+
+## v1.0.0-beta10
+
+* Warn if the image is not something we recognize. Fixes #11
+* Add support for specifying a base image. Fixes #11
+* Add a partial package config test
+
+## v1.0.0-beta9
+
+* Fix issue with the flipped behaviour of --no-push parameter
+
+## v1.0.0-beta8
+
+* Add support for any Docker registry. Fixes #10
+* Merge pull request #9 from axelerant/options-extra
+* Fix hash parameters for GH Actions caching
+* Add phpunit to Github Actions
+* Add tests for OptionsProvider
+* Document how to configure the plugin using the `extra` section
+* Support configuration from composer.json extra section. Fixes #8
+* Clarify the usage instructions in README
+* Fixes #5: Do not query remotes when reading information
+* Merge pull request #2 from axelerant/phplint
+* Add php lint and cs workflow
+* Add phplint
+* Use symfony/process for running commands
+* Reformat code to take lesser space
+* Fix comment in DbDocker
+* Enforce PSR-12 standards
+* Update CHANGELOG for beta7 release
+
+## v1.0.0-beta7
+
+* Correctly determine paths when building images
+
+## v1.0.0-beta6
+
+* Minor improvements
+* Remove a TODO after testing
+* Move the dockerize-db Dockerfile and SQL inside the plugin
+
+## v1.0.0-beta5
+
+* Remove shortcut for no-push
+
+## v1.0.0-beta4
+
+* Add support for composer 2.0
+* Merge pull request #1 from axelerant/arguments
+* Update single hyphen arugment name
---
+++ tests/OptionsProviderTest.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace Axelerant\DbDocker\Tests;
+
+use Axelerant\DbDocker\OptionsProvider;
+use Composer\Package\RootPackageInterface;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophet;
+use Symfony\Component\Console\Input\InputInterface;
+
+class OptionsProviderTest extends TestCase
+{
+    protected $prophet;
+
+    protected $input;
+
+    protected $package;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->prophet = new Prophet();
+        $this->input = $this->prophet->prophesize(InputInterface::class);
+        $this->package = $this->prophet->prophesize(RootPackageInterface::class);
+    }
+
+    /**
+     * @dataProvider dataOptions
+     */
+    public function testRegularOptions($input, $package, $expected): void
+    {
+        $this->input->getOption('docker-tag')->willReturn($input['docker-tag']);
+        $this->input->getOption('git-remote')->willReturn($input['git-remote']);
+        $this->input->getOption('db-source')->willReturn($input['db-source']);
+        $this->input->getOption('db-file')->willReturn($input['db-file']);
+        $this->input->getOption('no-push')->willReturn($input['no-push']);
+        $this->package->getExtra()->willReturn(['dbdocker' => $package]);
+
+        $options = new OptionsProvider($this->input->reveal(), $this->package->reveal());
+
+        $this->assertSame($expected['docker-image-name'], $options->getDockerImageName());
+        $this->assertSame($expected['docker-tag'], $options->getDockerTag());
+        $this->assertSame($expected['git-remote'], $options->getGitRemote());
+        $this->assertSame($expected['db-source'], $options->getDbSource());
+        $this->assertSame($expected['push'], $options->getPush());
+
+        $base = $options->getDockerBaseDetails();
+        $this->assertSame($expected['docker-base']['image'], $base['image']);
+        $this->assertSame($expected['docker-base']['user'], $base['user']);
+        $this->assertSame($expected['docker-base']['password'], $base['password']);
+        $this->assertSame($expected['docker-base']['database'], $base['database']);
+    }
+
+    public function dataOptions(): array
+    {
+        $defaultInput = [
+            'docker-tag' => '',
+            'git-remote' => '',
+            'db-source' => '',
+            'db-file' => '',
+            'no-push' => false,
+        ];
+        $defaultExpected = [
+            'docker-image-name' => 'auto',
+            'docker-tag' => 'auto',
+            'docker-base' => [
+                'base-flavor' => 'bitnami',
+                'image' => 'bitnami/mariadb:10.4',
+                'user' => 'drupal8',
+                'password' => 'drupal8',
+                'database' => 'drupal8',
+            ],
+            'git-remote' => 'origin',
+            'db-source' => '',
+            'push' => true,
+        ];
+
+        $cases = [];
+
+        // All defaults
+        $cases[] = [
+            'input' => $defaultInput,
+            'package' => [],
+            'expected' => $defaultExpected,
+        ];
+
+        // Configuration in package extra.
+        $cases[] = [
+            'input' => $defaultInput,
+            'package' => [
+                'docker-image-name' => 'test/image',
+                'docker-tag' => 'latest',
+                'git-remote' => 'upstream',
+                'db-source' => 'lando',
+                'no-push' => false,
+            ],
+            'expected' => [
+                'docker-image-name' => 'test/image',
+                'docker-tag' => 'latest',
+                'git-remote' => 'upstream',
+                'db-source' => 'lando',
+                'push' => true,
+            ] + $defaultExpected,
+        ];
+
+        // Configuration overridden on CLI.
+        $cases[] = [
+            'input' => [
+                'docker-tag' => 'production',
+                'git-remote' => 'upstream',
+                'db-source' => '',
+                'no-push' => true,
+            ] + $defaultInput,
+            'package' => [
+                'docker-image-name' => 'auto',
+                'docker-tag' => 'latest',
+                'git-remote' => '',
+                'db-source' => 'lando',
+                'no-push' => false,
+            ],
+            'expected' => [
+                'docker-image-name' => 'auto',
+                'docker-tag' => 'production',
+                'git-remote' => 'upstream',
+                'db-source' => 'lando',
+                'push' => false,
+            ] + $defaultExpected,
+        ];
+
+        // Partial configuration in composer.json.
+        $cases[] = [
+            'input' => [
+                'docker-tag' => 'production',
+                'git-remote' => 'upstream',
+                'db-source' => '',
+                'no-push' => true,
+            ] + $defaultInput,
+            'package' => [
+                'docker-tag' => 'latest',
+                'db-source' => 'lando',
+            ],
+            'expected' => [
+                'docker-image-name' => 'auto',
+                'docker-tag' => 'production',
+                'git-remote' => 'upstream',
+                'db-source' => 'lando',
+                'push' => false,
+            ] + $defaultExpected,
+        ];
+
+        // Input of db-file should always set the db-source option to 'file'.
+        // See https://github.com/axelerant/db-docker/issues/17.
+        $cases[] = [
+            'input' => [
+                'db-source' => '',
+                'db-file' => 'file.sql',
+            ] + $defaultInput,
+            'package' => [
+                'db-source' => 'lando',
+            ],
+            'expected' => [
+                'db-source' => 'file',
+            ] + $defaultExpected,
+        ];
+
+        // Configuration with docker-base.
+        $cases[] = [
+            'input' => $defaultInput,
+            'package' => [
+                'docker-base' => [
+                    'image' => 'mariadb:latest',
+                    'user' => 'mysql',
+                    'password' => 'root',
+                    'database' => 'drupal',
+                ],
+            ],
+            'expected' => [
+                'docker-base' => [
+                    'image' => 'mariadb:latest',
+                    'user' => 'mysql',
+                    'password' => 'root',
+                    'database' => 'drupal',
+                ],
+            ] + $defaultExpected,
+        ];
+
+        // Partial configuration with docker-base.
+        $cases[] = [
+            'input' => $defaultInput,
+            'package' => [
+                'docker-tag' => 'latest',
+                'docker-base' => [
+                    'image' => 'mariadb:latest',
+                ],
+                'db-source' => 'lando',
+            ],
+            'expected' => [
+                    'docker-tag' => 'latest',
+                    'docker-base' => [
+                        'image' => 'mariadb:latest',
+                        'user' => 'drupal8',
+                        'password' => 'drupal8',
+                        'database' => 'drupal8',
+                    ],
+                    'db-source' => 'lando',
+                ] + $defaultExpected,
+        ];
+
+        // Partial configuration with DDEV base flavor.
+        $cases[] = [
+            'input' => $defaultInput,
+            'package' => [
+                'docker-tag' => 'latest',
+                'docker-base' => [
+                    'base-flavor' => 'ddev',
+                ],
+                'db-source' => 'lando',
+            ],
+            'expected' => [
+                    'docker-tag' => 'latest',
+                    'docker-base' => [
+                        'base-flavor' => 'ddev',
+                        'image' => 'drud/ddev-dbserver-mariadb-10.4:v1.17.0',
+                        'user' => 'db',
+                        'password' => 'db',
+                        'database' => 'db',
+                    ],
+                    'db-source' => 'lando',
+                ] + $defaultExpected,
+        ];
+
+        return $cases;
+    }
+}
