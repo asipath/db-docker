cc3ef5dae04c1383ff1eba00a74ccc5c23d6442d
Asitha Sampath <raasampath@gmail.com>
2023-02-16 14:33:08 +0530

    master

--- src/DbDockerCommand.php
+++ src/DbDockerCommand.php
@@ -12,7 +12,6 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\ProcessBuilder;
 
-
 class DbDockerCommand extends BaseCommand
 {
     /**
