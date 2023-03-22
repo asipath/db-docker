310d779bbc4445aba0ba82ba081e9dde68ff196d
Asitha Sampath <raasampath@gmail.com>
2023-03-22 12:41:06 +0530

    php strom

--- src/testq.php
+++ src/testq.php
@@ -1,6 +1,6 @@
 <?php
 function remove_duplicate_address($array)
-    {
+{
     $full_address_string = '';
     foreach ($array as $val) {
         $address_array = explode(',', $val);
@@ -9,23 +9,23 @@ function remove_duplicate_address($array)
             //$half_address[]=$address_array[0];
             if (isset($address_array[1])) {
                 $half_address[trim($address_array[1])] = $val;
-                }
-            } else {
-            $full_address[]      = $val;
-            $full_address_string .= $val . '|';
             }
+        } else {
+            $full_address[] = $val;
+            $full_address_string .= $val . '|';
         }
+    }
 
     foreach ($half_address as $k => $v) {
         if (strpos($full_address_string, $k) !== false) {
             unset($half_address[$k]);
-            }
-
         }
-    return array_merge($full_address, $half_address);
+
     }
+    return array_merge($full_address, $half_address);
+}
 
 if (!empty($action) && $action <= 8) {
     $exclude_header_footer_content = true;
-    $include_popup[]               = "age_location_filter";
-    }
+    $include_popup[] = "age_location_filter";
+}
