5f5578787a53647b95adc7b5fd789c9ab9ae3b17
Asitha Sampath <raasampath@gmail.com>
2023-08-16 12:33:06 +0530

    trtr

--- src/testq.php
+++ src/testq.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * IDI API Class
  * Author : Social Catfish, LLC
@@ -7,11 +8,8 @@
  * Version : 1.0
  * Released Date : 04/04/2022
  */
- 
 namespace DataSource;
-
-class IDI extends \DataSource\Base 
-{
+class IDI extends \DataSource\Base {
     public const SDK_VERSION = 1.0;
     public const AUTH_VALIDITY = 1680;
     public const API_ENDPOINT_AUTH = "https://login-api.idicore.com/apiclient";
